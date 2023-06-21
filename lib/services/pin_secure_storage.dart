b53351d5e1735996d67d5d2237e74f41c3bc8437
b3njah <benjahjp@gmail.com>
2023-06-21 17:09:08 +0300

    feat: pin login

---
+++ lib/services/pin_secure_storage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final TextEditingController _userNameController = TextEditingController();
+
+class PinSecureStorage {
+  // Create storage
+  final storage = const FlutterSecureStorage();
+
+  late final String _keyPin;
+
+  Future setUserName(String pin) async {
+    await storage.write(key: _keyPin, value: pin);
+  }
+
+  Future<String?> getUserName() async {
+    return await storage.read(key: _keyPin);
+  }
+}
