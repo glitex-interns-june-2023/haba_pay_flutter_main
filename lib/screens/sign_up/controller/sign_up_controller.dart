45bea5fb7cfb820802ebec456e07ef25f849c86d
b3njah <benjahjp@gmail.com>
2023-06-22 12:29:20 +0300

    feat: sign up and sign in api intergration

--- lib/screens/sign_up/controller/sign_up_controller.dart
+++ lib/screens/sign_up/controller/sign_up_controller.dart
@@ -1,10 +1,16 @@
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:haba_pay_main/model/User.dart';
+import 'package:haba_pay_main/model/sign_in_entity.dart';
+import 'package:haba_pay_main/services/base_client.dart';
 import 'package:haba_pay_main/services/pin_secure_storage.dart';
 
+import '../../dashboard/components/dashboard.dart';
 import '../components/add_phone_number.dart';
 
 class SignUpController extends GetxController{
+  var userInfo = User().obs;
+  var user = SignInEntity().obs;
   final SecureStorage _secureStorage = SecureStorage();
   var isLoading = false.obs;
   final _googleSignIn = GoogleSignIn();
@@ -35,9 +41,22 @@ class SignUpController extends GetxController{
   sendUserInfo() async{
     isLoading(true);
     try{
-      //post data
-      //get token and store it locally
-    } finally{
+      userInfo(
+       User(
+         username: await _secureStorage.getUserName(),
+         clientId: await _secureStorage.getClientId(),
+         email: await _secureStorage.getEmail(),
+         phoneNumber: await _secureStorage.getPhoneNumber()
+       )
+      );
+      var response = await BaseClient.post(
+          "/sign_up",
+          userInfo
+      );
+      user(SignInEntity.fromJson(response));
+      await _secureStorage.setAuthToken(user.value.accessToken);
+      Get.to(()=> const Dashboard(), transition: Transition.rightToLeft);
+    } finally {
       isLoading(false);
     }
   }
