02882e3d2b1c4f0e42510ae39271c47a4cfd37f9
b3njah <benjahjp@gmail.com>
2023-08-15 16:34:51 +0300

    feat: sendmoney

---
+++ lib/screens/send_money/components/confirm_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ConfirmDetails extends StatefulWidget {
+  const ConfirmDetails({super.key});
+
+  @override
+  State<ConfirmDetails> createState() => _ConfirmDetailsState();
+}
+
+class _ConfirmDetailsState extends State<ConfirmDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
