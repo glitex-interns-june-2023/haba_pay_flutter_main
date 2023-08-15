6aaa45d87a8cfba94ffc313ae17a3f3b3acb82b4
b3njah <benjahjp@gmail.com>
2023-08-15 23:52:29 +0300

    feat: fix phone size

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppPage.getCreateLoginPin(),
+      initialRoute: AppPage.getSignUp(),
       getPages: AppPage.routes,
     ),
   );
