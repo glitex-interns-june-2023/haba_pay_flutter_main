2b8d15d04af025c95fd48ffd13acdfee3610de70
b3njah <benjahjp@gmail.com>
2023-08-18 21:22:10 +0300

    feat: finalizing

--- lib/screens/faqs_and_tcs/components/tcs.dart
+++ lib/screens/faqs_and_tcs/components/tcs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:haba_pay_main/Theme/custom_theme.dart';
+import 'package:haba_pay_main/screens/Shared/CustomAppBar.dart';
 import '../../Shared/title_text.dart';
 import '../controller/faqs_and_tcs_controller.dart';
 import 'package:get/get.dart';
@@ -12,37 +13,44 @@ class Tcs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              const Spacer(),
-              const TitleText(title: "Terms and conditions"),
-              const SizedBox(height: 20,),
-              Expanded(
-                  flex: 5,
-                  child: ListView.builder(
-                      itemCount: faqsAndTcsController.tcList.length,
-                      itemBuilder: (context, index){
-                        return Obx(() =>
-                            ExpansionTile(
-                              title: Text(faqsAndTcsController.tcList[index].title),
-                              onExpansionChanged: (expanded){
-                                faqsAndTcsController.toggleTcItem(index);
-                              },
-                              initiallyExpanded: faqsAndTcsController.expandedTcItems[index] ?? false,
-                              children: [
-                                ListTile(
-                                  title: Text(faqsAndTcsController.tcList[index].description),
-                                )
-                              ],
-                            )
-                        );
-                      }
-                  )
-              ),
-            ],
+      backgroundColor: theme.background,
+      appBar: const CustomAppBar(title: "Terms and conditions"),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                const Spacer(),
+                const TitleText(title: "Terms and conditions"),
+                const SizedBox(height: 20,),
+                Expanded(
+                    flex: 5,
+                    child: ListView.builder(
+                        itemCount: faqsAndTcsController.tcList.length,
+                        itemBuilder: (context, index){
+                          return Obx(() =>
+                              ExpansionTile(
+                                iconColor: theme.black,
+                                textColor: theme.black,
+                                title: Text(faqsAndTcsController.tcList[index].title),
+                                onExpansionChanged: (expanded){
+                                  faqsAndTcsController.toggleTcItem(index);
+                                },
+                                initiallyExpanded: faqsAndTcsController.expandedTcItems[index] ?? false,
+                                children: [
+                                  ListTile(
+                                    title: Text(faqsAndTcsController.tcList[index].description),
+                                  )
+                                ],
+                              )
+                          );
+                        }
+                    )
+                ),
+              ],
+            ),
           ),
         ),
       ),
