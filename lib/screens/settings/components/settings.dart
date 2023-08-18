643585be547ff301ee638ac2341daaaeed239aaf
b3njah <benjahjp@gmail.com>
2023-08-18 21:32:55 +0300

    feat: finalizing

--- lib/screens/settings/components/settings.dart
+++ lib/screens/settings/components/settings.dart
@@ -49,15 +49,15 @@ class Settings extends StatelessWidget {
       ),
       body: LayoutBuilder(
         builder: (context, constraint){
-          return Padding(
-            padding: const EdgeInsets.all(16),
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: SingleChildScrollView(
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(minHeight: constraint.maxHeight),
-                    child: IntrinsicHeight(
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraint.maxHeight),
+              child: IntrinsicHeight(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
