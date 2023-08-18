7681d9f72f6092488476304d41fa729edde9ca29
b3njah <benjahjp@gmail.com>
2023-08-18 14:02:09 +0300

    feat: finalizing

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppPage.getCreateLoginPin(),
+      initialRoute: AppPage.getConfirmLoginPin(),
       getPages: AppPage.routes,
     ),
   );
