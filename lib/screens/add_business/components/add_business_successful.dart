28a2bfe289c123e7f8dc592d58f8efd5cb6b9b0a
b3njah <benjahjp@gmail.com>
2023-08-19 11:55:12 +0300

    feat: finalizing

--- lib/screens/add_business/components/add_business_successful.dart
+++ lib/screens/add_business/components/add_business_successful.dart
@@ -23,79 +23,82 @@ class AddBusinessSuccessful extends StatelessWidget {
               child: ConstrainedBox(
                   constraints: BoxConstraints(minHeight: constraint.maxHeight),
                 child: IntrinsicHeight(
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          const Spacer(),
-                          const Text(
-                            "Process successful",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          const Spacer(),
-                          SvgPicture.asset(
-                            'assets/images/smile_face.svg',
-                            color: theme.orange,
-                          ),
-                          const Spacer(),
-                          const Text(
-                            "Offers will be customized to your \n business and location.",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontSize: 18
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text(
-                                "Read about our ",
-                                style: TextStyle(
-                                  fontSize: 18
-                                ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          children: [
+                            const Spacer(),
+                            const Text(
+                              "Process successful",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold
                               ),
-                              InkWell(
-                                onTap: (){},
-                                child: Text(
-                                    "data privacy",
+                            ),
+                            const Spacer(),
+                            SvgPicture.asset(
+                              'assets/images/smile_face.svg',
+                              color: theme.orange,
+                            ),
+                            const Spacer(),
+                            const Text(
+                              "Offers will be customized to your \n business and location.",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 18
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  "Read about our ",
                                   style: TextStyle(
-                                    fontSize: 18,
-                                    color: theme.orange
+                                    fontSize: 18
                                   ),
                                 ),
-                              )
-                            ],
-                          ),
-                          const Spacer(
-                            flex: 2,
-                          ),
-                          MaterialButton(
-                              onPressed: () {
-                                Get.offAll(
-                                      () => const Dashboard(),
-                                  transition: Transition.rightToLeft,
-                                );
-                              },
-                              height: 50,
-                              minWidth: double.infinity,
-                              color: theme.orange,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 50),
-                                child: Text(
-                                  "Go to Home",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: theme.white,
-                                      fontSize: 20),
-                                ),
-                              )
-                          ),
-                          const Spacer(),
-                        ],
+                                InkWell(
+                                  onTap: (){},
+                                  child: Text(
+                                      "data privacy",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: theme.orange
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            const Spacer(
+                              flex: 2,
+                            ),
+                            MaterialButton(
+                                onPressed: () {
+                                  Get.offAll(
+                                        () => const Dashboard(),
+                                    transition: Transition.rightToLeft,
+                                  );
+                                },
+                                height: 50,
+                                minWidth: double.infinity,
+                                color: theme.orange,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                                  child: Text(
+                                    "Go to Home",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: theme.white,
+                                        fontSize: 20),
+                                  ),
+                                )
+                            ),
+                            const Spacer(),
+                          ],
+                        ),
                       ),
                     ),
                   ),
