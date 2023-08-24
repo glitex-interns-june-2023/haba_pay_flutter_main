41bacaaad952e3a8bd7fdf087c6291d3cf2d7814
b3njah <benjahjp@gmail.com>
2023-08-24 15:45:34 +0300

    feat: quick login

--- lib/screens/pin_login/components/pin_login.dart
+++ lib/screens/pin_login/components/pin_login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:haba_pay_main/Theme/custom_theme.dart';
+import 'package:haba_pay_main/screens/Shared/custom_button.dart';
 import 'package:haba_pay_main/screens/pin_login/components/single_button.dart';
 import 'package:haba_pay_main/screens/pin_login/components/single_pin_field.dart';
 import 'package:haba_pay_main/screens/pin_login/controllers/pin_login_controller.dart';
@@ -19,175 +20,170 @@ class PinLogin extends StatelessWidget {
             child: ConstrainedBox(
               constraints: BoxConstraints(minHeight: constraint.maxHeight),
               child: IntrinsicHeight(
-                child: Column(
-                  children: [
-                    const Spacer(
-                      flex: 2,
-                    ),
-                    const Text(
-                      "Good afternoon, Brian",
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                    const Spacer(
-                      flex: 2,
-                    ),
-                    Obx(() => Row(
-                          children: [
-                            const Spacer(),
-                            if (pinLoginController.pin.isNotEmpty)
-                               SinglePinField(color: theme.orange)
-                            else
-                               SinglePinField(color: theme.grey),
-                            const Spacer(),
-                            if (pinLoginController.pin.isNotEmpty &&
-                                pinLoginController.pin.length > 1)
-                               SinglePinField(color: theme.orange)
-                            else
-                               SinglePinField(color: theme.grey),
-                            const Spacer(),
-                            if (pinLoginController.pin.isNotEmpty &&
-                                pinLoginController.pin.length > 2)
-                               SinglePinField(color: theme.orange)
-                            else
-                               SinglePinField(color: theme.grey),
-                            const Spacer(),
-                            if (pinLoginController.pin.isNotEmpty &&
-                                pinLoginController.pin.length > 3)
-                               SinglePinField(color: theme.orange)
-                            else
-                               SinglePinField(color: theme.grey),
-                            const Spacer(),
-                          ],
-                        )),
-                    const Spacer(
-                      flex: 2,
-                    ),
-                    Row(
-                      children: [
-                        const Spacer(),
-                        SingleButton(
-                          number: "1",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(1);
-                          },
-                        ),
-                        const Spacer(),
-                        SingleButton(
-                          number: "2",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(2);
-                          },
-                        ),
-                        const Spacer(),
-                        SingleButton(
-                          number: "3",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(3);
-                          },
-                        ),
-                        const Spacer()
-                      ],
-                    ),
-                    const Spacer(),
-                    Row(
-                      children: [
-                        const Spacer(),
-                        SingleButton(
-                          number: "4",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(4);
-                          },
-                        ),
-                        const Spacer(),
-                        SingleButton(
-                          number: "5",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(5);
-                          },
-                        ),
-                        const Spacer(),
-                        SingleButton(
-                          number: "6",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(6);
-                          },
-                        ),
-                        const Spacer()
-                      ],
-                    ),
-                    const Spacer(),
-                    Row(
-                      children: [
-                        const Spacer(),
-                        SingleButton(
-                          number: "7",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(7);
-                          },
-                        ),
-                        const Spacer(),
-                        SingleButton(
-                          number: "8",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(8);
-                          },
-                        ),
-                        const Spacer(),
-                        SingleButton(
-                          number: "9",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(9);
-                          },
-                        ),
-                        const Spacer()
-                      ],
-                    ),
-                    const Spacer(),
-                    Row(
-                      children: [
-                        const Spacer(),
-                        const SizedBox(
-                          width: 80,
-                        ),
-                        const Spacer(),
-                        SingleButton(
-                          number: "0",
-                          onPress: () {
-                            pinLoginController.onNumberClicked(0);
-                          },
-                        ),
-                        const Spacer(),
-                        MaterialButton(
-                            focusElevation: 0,
-                            hoverElevation: 0,
-                            highlightElevation: 0,
-                            shape: const CircleBorder(),
-                            onPressed: () {
-                              pinLoginController.onClearLast();
-                            },
-                            elevation: 0,
-                            color: theme.background,
-                            child: const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Icon(Icons.clear))),
-                        const Spacer()
-                      ],
-                    ),
-                    const Spacer(),
-                    const SizedBox(height: 20,),
-                    Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: MaterialButton(
-                        onPressed: () {},
-                        minWidth: double.infinity,
-                        height: 50,
-                        color: theme.orange,
-                        child:  Text("Log in",
-                            style: TextStyle(color: theme.white, fontSize: 16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      const Spacer(
+                        flex: 2,
                       ),
-                    ),
-                    const Spacer()
-                  ],
+                      const Text(
+                        "Good afternoon, Brian",
+                        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(
+                        flex: 2,
+                      ),
+                      Obx(() => Row(
+                            children: [
+                              const Spacer(),
+                              if (pinLoginController.pin.isNotEmpty)
+                                 SinglePinField(color: theme.orange)
+                              else
+                                 SinglePinField(color: theme.grey),
+                              const Spacer(),
+                              if (pinLoginController.pin.isNotEmpty &&
+                                  pinLoginController.pin.length > 1)
+                                 SinglePinField(color: theme.orange)
+                              else
+                                 SinglePinField(color: theme.grey),
+                              const Spacer(),
+                              if (pinLoginController.pin.isNotEmpty &&
+                                  pinLoginController.pin.length > 2)
+                                 SinglePinField(color: theme.orange)
+                              else
+                                 SinglePinField(color: theme.grey),
+                              const Spacer(),
+                              if (pinLoginController.pin.isNotEmpty &&
+                                  pinLoginController.pin.length > 3)
+                                 SinglePinField(color: theme.orange)
+                              else
+                                 SinglePinField(color: theme.grey),
+                              const Spacer(),
+                            ],
+                          )),
+                      const Spacer(
+                        flex: 2,
+                      ),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          SingleButton(
+                            number: "1",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(1);
+                            },
+                          ),
+                          const Spacer(),
+                          SingleButton(
+                            number: "2",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(2);
+                            },
+                          ),
+                          const Spacer(),
+                          SingleButton(
+                            number: "3",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(3);
+                            },
+                          ),
+                          const Spacer()
+                        ],
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          SingleButton(
+                            number: "4",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(4);
+                            },
+                          ),
+                          const Spacer(),
+                          SingleButton(
+                            number: "5",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(5);
+                            },
+                          ),
+                          const Spacer(),
+                          SingleButton(
+                            number: "6",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(6);
+                            },
+                          ),
+                          const Spacer()
+                        ],
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          SingleButton(
+                            number: "7",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(7);
+                            },
+                          ),
+                          const Spacer(),
+                          SingleButton(
+                            number: "8",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(8);
+                            },
+                          ),
+                          const Spacer(),
+                          SingleButton(
+                            number: "9",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(9);
+                            },
+                          ),
+                          const Spacer()
+                        ],
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          const SizedBox(
+                            width: 80,
+                          ),
+                          const Spacer(),
+                          SingleButton(
+                            number: "0",
+                            onPress: () {
+                              pinLoginController.onNumberClicked(0);
+                            },
+                          ),
+                          const Spacer(),
+                          MaterialButton(
+                              focusElevation: 0,
+                              hoverElevation: 0,
+                              highlightElevation: 0,
+                              shape: const CircleBorder(),
+                              onPressed: () {
+                                pinLoginController.onClearLast();
+                              },
+                              elevation: 0,
+                              color: theme.background,
+                              child: const Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: Icon(Icons.clear))),
+                          const Spacer()
+                        ],
+                      ),
+                      const Spacer(),
+                      const SizedBox(height: 20,),
+                      CustomButton(title: "Log in", onClick: (){
+                        pinLoginController.confirmLoginPin();
+                      }),
+                      const Spacer()
+                    ],
+                  ),
                 ),
               ),
             ),
