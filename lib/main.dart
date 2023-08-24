9774110bd119a22875282b0c7e6694de33025867
b3njah <benjahjp@gmail.com>
2023-08-24 15:41:55 +0300

    feat: quick login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:haba_pay_main/routes/app_page.dart';
-import 'package:haba_pay_main/services/pin_secure_storage.dart';
 
 void main() {
   runApp(
--- lib/screens/loading/Loading.dart
+++ lib/screens/loading/Loading.dart
@@ -3,15 +3,18 @@ import 'package:haba_pay_main/Theme/custom_theme.dart';
 import 'package:haba_pay_main/screens/loading/loading_controller.dart';
 
 final CustomTheme theme = CustomTheme();
-final LoadingController loadingController = LoadingController();
 class Loading extends StatelessWidget {
   const Loading({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircularProgressIndicator(
-        color: theme.orange,
+    final LoadingController loadingController = LoadingController();
+    loadingController.onInit();
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(
+          color: theme.orange,
+        ),
       ),
     );
   }
