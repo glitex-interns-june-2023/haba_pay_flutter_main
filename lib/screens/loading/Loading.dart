e7c70b63ff54f668a98356c43df8263f6e974a6a
b3njah <benjahjp@gmail.com>
2023-08-24 14:16:18 +0300

    feat: quick login

--- lib/screens/loading/Loading.dart
+++ lib/screens/loading/Loading.dart
@@ -8,8 +8,10 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CircularProgressIndicator(
-        color: theme.orange,
+      body: Center(
+        child: CircularProgressIndicator(
+          color: theme.orange,
+        ),
       ),
     );
   }
