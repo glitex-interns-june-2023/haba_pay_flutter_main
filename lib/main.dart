bc140f026d73b3f03be4a92abad632c9f8c1821b
b3njah <benjahjp@gmail.com>
2023-08-24 14:14:28 +0300

    feat: quick login

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: AppPage.startPage().toString(),
+      initialRoute: AppPage.loading,
       getPages: AppPage.routes,
     ),
   );
--- lib/screens/loading/controller/LoadingController.dart
+++ lib/screens/loading/controller/LoadingController.dart
@@ -1,5 +1,30 @@
 import 'package:get/get.dart';
+import 'package:haba_pay_main/screens/pin_login/components/pin_login.dart';
+import 'package:haba_pay_main/screens/sign_up/components/sign_up.dart';
+import '../../../services/pin_secure_storage.dart';
+
+final SecureStorage _secureStorage = SecureStorage();
 
 class LoadingController extends GetxController{
+  var loading = false.obs;
 
+  @override
+  Future<void> onInit() async {
+    super.onInit();
+    loading.value = true;
+    try{
+      var loginWithPin = false;
+      var quickLogin = await _secureStorage.getQuickLogin();
+      quickLogin == "true" ? true : false;
+      if(loginWithPin == true){
+        Get.to(() => const PinLogin(),
+            transition: Transition.rightToLeft);
+      } else {
+        Get.to(() => const SignUp(),
+            transition: Transition.rightToLeft);
+      }
+    } finally {
+      loading.value = false;
+    }
+  }
 }
