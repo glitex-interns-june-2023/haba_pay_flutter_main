1a8d16e5fd9ce5d3c5d292c1a01a5d97835f1c01
b3njah <benjahjp@gmail.com>
2023-09-01 19:09:37 +0300

    feat: deposit money controller

---
+++ lib/model/DepositModel.dart
@@ -0,0 +1,10 @@
+class DepositModel {
+  String senderPhone;
+  String receiverPhone;
+  String amount;
+
+  DepositModel(
+      {required this.amount,
+      required this.senderPhone,
+      required this.receiverPhone});
+}
