a57f3398f154afe804a38dbf06b534004049cf63
b3njah <benjahjp@gmail.com>
2023-09-01 21:00:56 +0300

    feat: deposit money

--- lib/screens/deposit_money/components/deposit_confirm_details.dart
+++ lib/screens/deposit_money/components/deposit_confirm_details.dart
@@ -73,17 +73,15 @@ class DepositConfirmDetails extends StatelessWidget {
                                       fontSize: 18),
                                 )),
                           ),
-                          Obx(
-                            () => Align(
-                              alignment: Alignment.topLeft,
-                              child: Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 8),
-                                  child: Text(
-                                    depositMoneyController.myNumber.value,
-                                    style: const TextStyle(fontSize: 18),
-                                  )),
-                            ),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8),
+                                child: Text(
+                                  depositMoneyModel.phoneNumber,
+                                  style: const TextStyle(fontSize: 18),
+                                )),
                           ),
                           const Align(
                             alignment: Alignment.topLeft,
@@ -156,14 +154,13 @@ class DepositConfirmDetails extends StatelessWidget {
                                 width: 10,
                               ),
                               Expanded(
-                                  child: CustomButton(
-                                      title: "Send",
-                                      onClick: () {
-                                        depositMoneyController.send(
-                                          depositMoneyModel.phoneNumber,
-                                          depositMoneyModel.amount,
-                                        );
-                                      })),
+                                child: CustomButton(title: "Send", onClick: (){
+                                  depositMoneyController.send(
+                                    depositMoneyModel.phoneNumber,
+                                    depositMoneyModel.amount,
+                                  );
+                                })
+                              ),
                             ],
                           ),
                           const Spacer()
