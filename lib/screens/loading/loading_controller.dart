5110c41f04b0fe1786198cbe157ab7c0f438b4ee
b3njah <benjahjp@gmail.com>
2023-09-04 13:35:10 +0300

    feat: auth flow bug fix

--- lib/screens/loading/loading_controller.dart
+++ lib/screens/loading/loading_controller.dart
@@ -5,23 +5,28 @@ import 'package:haba_pay_main/screens/sign_up/components/sign_up.dart';
 import 'package:haba_pay_main/services/pin_secure_storage.dart';
 
 final SecureStorage _secureStorage = SecureStorage();
-class LoadingController extends GetxController{
+
+class LoadingController extends GetxController {
   @override
   Future<void> onInit() async {
     super.onInit();
     var accessToken = await _secureStorage.getAuthToken();
     var quickLogin = await _secureStorage.getQuickLogin();
-    if(quickLogin != null && quickLogin == "true" && accessToken != null && accessToken.isNotEmpty){
-      Get.offAll(() => const PinLogin(),
-          transition: Transition.rightToLeft);
-    } else if (accessToken != null && accessToken.isNotEmpty) {
-      Get.offAll(
-          const Dashboard(),
-          transition: Transition.rightToLeft
-      );
-    }else{
-        Get.offAll(() => const SignUp(),
-            transition: Transition.rightToLeft);
+    var isPhoneVerified = await _secureStorage.getIsPhoneVerified();
+    if (quickLogin != null &&
+        quickLogin == "true" &&
+        accessToken != null &&
+        accessToken.isNotEmpty &&
+        isPhoneVerified != null &&
+        isPhoneVerified == "true") {
+      Get.offAll(() => const PinLogin(), transition: Transition.rightToLeft);
+    } else if (accessToken != null &&
+        accessToken.isNotEmpty &&
+        isPhoneVerified != null &&
+        isPhoneVerified == "true") {
+      Get.offAll(const Dashboard(), transition: Transition.rightToLeft);
+    } else {
+      Get.offAll(() => const SignUp(), transition: Transition.rightToLeft);
     }
   }
 }
