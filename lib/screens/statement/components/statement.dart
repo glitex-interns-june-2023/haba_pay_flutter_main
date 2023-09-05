f4d666e22a50d8f7813872673087e9d18b019d42
b3njah <benjahjp@gmail.com>
2023-09-05 18:18:27 +0300

    feat: is empty list

--- lib/screens/statement/components/statement.dart
+++ lib/screens/statement/components/statement.dart
@@ -128,84 +128,99 @@ class Statement extends StatelessWidget {
                 child: Card(
                   child: Padding(
                       padding: const EdgeInsets.all(16),
-                      child: Obx(() => statementController.isLoading.value
-                          ? Center(
-                              child: CircularProgressIndicator(
-                              color: theme.orange,
-                            ))
-                          : ListView.builder(
-                              controller:
-                                  statementController.scrollController,
-                              itemCount:
-                                  statementController.isLoadingMore.value
-                                      ? statementController.list.length + 1
-                                      : statementController.list.length,
-                              itemBuilder: (context, index) {
-                                if (index < statementController.list.length) {
-                                  return Column(
-                                    children: [
-                                      Align(
-                                          alignment: Alignment.topLeft,
-                                          child: Text(statementController
-                                              .list[index].date)),
-                                      const Divider(),
-                                      for (var transaction
-                                          in statementController
-                                              .list[index].statementList)
-                                        SingleStatement(
-                                            type: transaction['type'],
-                                            onClick: () {
-                                              Get.to(
-                                                  () =>
-                                                      const TransactionDetails(),
-                                                  transition:
-                                                      Transition.rightToLeft,
-                                                  arguments: {
-                                                    'transaction_id':
-                                                        transaction[
-                                                            'transaction_id'],
-                                                    'type':
-                                                        transaction['type'],
-                                                    'name': transaction[
-                                                        'full_name'],
-                                                    'phone':
-                                                        transaction['phone'],
-                                                    'amount':
-                                                        transaction['amount'],
-                                                    'timestamp': transaction[
-                                                        'timestamp'],
-                                                    'date':
-                                                        statementController
-                                                            .list[index].date
-                                                  });
-                                            },
-                                            name: transaction['full_name'],
-                                            phoneNumber: transaction['phone'],
-                                            amount: transaction['amount'],
-                                            time: transaction['timestamp'])
-                                    ],
-                                  );
-                                } else {
-                                  return Center(
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(vertical: 16),
-                                      child: statementController.hasMore.value ?
-                                          CircularProgressIndicator(
-                                            color: theme.orange,
-                                          )
-                                          :
-                                          Text(
-                                            "No more transactions to load",
-                                            style: TextStyle(
-                                              color: theme.orange,
-                                              fontWeight: FontWeight.bold
-                                            ),
-                                          )
-                                      ,
-                                    ),
-                                  );
-                                }
-                              }))),
+                      child: Column(
+                        children: [
+                          if(statementController.list.isEmpty)
+                            Center(
+                              child: Text(
+                                  "No Transactions to display",
+                                style: TextStyle(
+                                  color: theme.orange,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            )
+                          else
+                          Obx(() => statementController.isLoading.value
+                              ? Center(
+                                  child: CircularProgressIndicator(
+                                  color: theme.orange,
+                                ))
+                              : ListView.builder(
+                                  controller:
+                                      statementController.scrollController,
+                                  itemCount:
+                                      statementController.isLoadingMore.value
+                                          ? statementController.list.length + 1
+                                          : statementController.list.length,
+                                  itemBuilder: (context, index) {
+                                    if (index < statementController.list.length) {
+                                      return Column(
+                                        children: [
+                                          Align(
+                                              alignment: Alignment.topLeft,
+                                              child: Text(statementController
+                                                  .list[index].date)),
+                                          const Divider(),
+                                          for (var transaction
+                                              in statementController
+                                                  .list[index].statementList)
+                                            SingleStatement(
+                                                type: transaction['type'],
+                                                onClick: () {
+                                                  Get.to(
+                                                      () =>
+                                                          const TransactionDetails(),
+                                                      transition:
+                                                          Transition.rightToLeft,
+                                                      arguments: {
+                                                        'transaction_id':
+                                                            transaction[
+                                                                'transaction_id'],
+                                                        'type':
+                                                            transaction['type'],
+                                                        'name': transaction[
+                                                            'full_name'],
+                                                        'phone':
+                                                            transaction['phone'],
+                                                        'amount':
+                                                            transaction['amount'],
+                                                        'timestamp': transaction[
+                                                            'timestamp'],
+                                                        'date':
+                                                            statementController
+                                                                .list[index].date
+                                                      });
+                                                },
+                                                name: transaction['full_name'],
+                                                phoneNumber: transaction['phone'],
+                                                amount: transaction['amount'],
+                                                time: transaction['timestamp'])
+                                        ],
+                                      );
+                                    } else {
+                                      return Center(
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(vertical: 16),
+                                          child: statementController.hasMore.value ?
+                                              CircularProgressIndicator(
+                                                color: theme.orange,
+                                              )
+                                              :
+                                              Text(
+                                                "No more transactions to load",
+                                                style: TextStyle(
+                                                  color: theme.orange,
+                                                  fontWeight: FontWeight.bold
+                                                ),
+                                              )
+                                          ,
+                                        ),
+                                      );
+                                    }
+                                  })),
+                        ],
+                      )),
                 ),
               ),
             )
